13ab01db79e63c0d3b57d4aeecbae69c6d0e0c86
monis_khan <monisykhan@gmail.com>
2024-12-19 22:55:42 +0530

    updated the auth middleware

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,26 +1,25 @@
 import { domainToUnicode } from "url";
-import ErrorHanlder from "../utils/Error"
-import jwt from 'jsonwebtoken'
+import ErrorHanlder from "../utils/Error";
+import jwt from 'jsonwebtoken';
 import { NextFunction, Request, Response } from "express";
 
-
-
 export const authentication = async (req: Request, res: Response, next: NextFunction) => {
-
     try {
+        const token = typeof req.headers.token === "string" ? req.headers.token : undefined;
 
-        const { token } = req.headers
         if (!token) return next(new ErrorHanlder(404, 'Token not found'));
-        const decoded_token = jwt.decode(token)
-        if (!decoded_token) return next(new ErrorHanlder(404, 'Invalid token'));
+        
+        const decoded_token = jwt.decode(token) as { clerkId?: string } | null;
+
+        if (!decoded_token || !decoded_token.clerkId) {
+            return next(new ErrorHanlder(404, 'Invalid token'));
+        }
 
         req.body.clerkId = decoded_token.clerkId;
-        next()
+        next();
 
-    } catch (error) {
-        console.log('error:', error)
-        new ErrorHanlder(404, error.message);
-
+    } catch (error: any) {
+        console.log('error:', error);
+        return next(new ErrorHanlder(404, error.message));
     }
-
-}
+};
